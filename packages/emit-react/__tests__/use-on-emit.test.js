8683242a9621385874adce36fda4c5d15334343d
Tanner <johntannerlehett@gmail.com>
2022-10-18 23:04:07 -0700

    Complete initial work on the @psionic/emit-react package.

--- packages/emit-react/__tests__/use-on-emit.test.js
+++ packages/emit-react/__tests__/use-on-emit.test.js
@@ -1,24 +1,153 @@
+import { useState } from 'react';
 import { fireEvent, render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
+import { emit } from '@psionic/emit';
+import { useOnEmit } from '../lib';
 
-console.log(expect);
-describe('useOnEmit Hook Tests', () => {
+test('is able to create a listener and have it automatically cancel when the component\'s lifecycle is complete', async () => {
+    let value = 0;
 
-    it('is able to create a listener', async () => {
-        let value = 0;
+    const Listener = () => {
+        useOnEmit('increment', () => value++);
 
-        const ComponentWithListener = () => {
-            return <button onClick={() => value++}>
-                Hello there!
-            </button>
-        };
+        return null;
+    };
 
-        render(<ComponentWithListener/>);
+    const IncrementButton = () => {
+        return <button onClick={() => {
+            emit('increment');
+            emit('check');
+        }}>Increment Count</button>;
+    };
 
-        fireEvent.click(screen.getByText('Hello there!'));
+    const Wrapper = () => {
+        const [showListener, setShowListener] = useState(true);
 
-        expect(screen.getByRole('heading')).toHaveTextContent('Hello there!');
+        useOnEmit('check', () => {
+            if (value >= 2) {
+                setShowListener(false);
+            }
+        });
 
-    });
+        return showListener
+            ? (
+                <>
+                    <Listener/>
+                    <IncrementButton/>
+                </>
+            )
+            : <IncrementButton/>;
+    };
 
+
+    render(<Wrapper/>);
+
+    const incrementButton = screen.getByRole('button');
+
+    fireEvent.click(incrementButton);
+    fireEvent.click(incrementButton);
+    // This last one should not actually increment the value, since the listener is no longer being rendered
+    fireEvent.click(incrementButton);
+
+    expect(value).toEqual(2);
+});
+
+test('is able to cancel the created listener early', async () => {
+    let value = 0;
+
+    const Listener = () => {
+        const [startListener, cancelListener, listenerIsActive] = useOnEmit('increment', () => {
+            value++;
+            cancelListener();
+        });
+
+        return null;
+    };
+
+    const IncrementButton = () => {
+        return <button onClick={() => emit('increment')}>Increment Count</button>;
+    };
+
+    const Wrapper = () => (
+        <>
+            <Listener/>
+            <IncrementButton/>
+        </>
+    );
+
+
+    render(<Wrapper/>);
+
+    const incrementButton = screen.getByRole('button');
+
+    fireEvent.click(incrementButton);
+    // These events should not actually increment the value, since the listener has been canceled early
+    fireEvent.click(incrementButton);
+    fireEvent.click(incrementButton);
+
+    expect(value).toEqual(1);
+});
+
+test('is able to restart canceled listeners', async () => {
+    let value = 0;
+    let active = true;
+
+    const Listener = () => {
+        const [startListener, cancelListener, listenerIsActive] = useOnEmit('increment', () => {
+            value++;
+        });
+
+        active = listenerIsActive;
+
+        return <button onClick={() => listenerIsActive ? cancelListener() : startListener()}>
+            Toggle Listener
+        </button>;
+    };
+
+    const IncrementButton = () => {
+        return <button onClick={() => {
+            emit('increment');
+            emit('check');
+        }}>Increment</button>;
+    };
+
+    const Wrapper = () => {
+        const [showListener, setShowListener] = useState(true);
+
+        useOnEmit('check', () => {
+            if (value >= 2) {
+                setShowListener(false);
+            }
+        });
+
+        return showListener
+            ? (
+                <>
+                    <Listener/>
+                    <IncrementButton/>
+                </>
+            )
+            : <IncrementButton/>;
+    };
+
+    render(<Wrapper/>);
+
+    const incrementButton = screen.getByText('Increment');
+    const toggleListenerButton = screen.getByText('Toggle Listener');
+
+    fireEvent.click(incrementButton);
+    fireEvent.click(toggleListenerButton);
+    fireEvent.click(incrementButton);
+    fireEvent.click(incrementButton);
+    fireEvent.click(incrementButton);
+    fireEvent.click(incrementButton);
+    fireEvent.click(toggleListenerButton);
+    fireEvent.click(incrementButton);
+    fireEvent.click(incrementButton);
+    fireEvent.click(incrementButton);
+    fireEvent.click(incrementButton);
+    fireEvent.click(incrementButton);
+    fireEvent.click(incrementButton);
+
+    expect(value).toEqual(2);
 });
---
+++ packages/emit-react/src/index.js
@@ -0,0 +1,8 @@
+/**
+ * React hooks for integrating with the @psionic/emit library.
+ *
+ * @public
+ * @module @psionic/emit-react
+ */
+
+export * from './use-on-emit/use-on-emit';
